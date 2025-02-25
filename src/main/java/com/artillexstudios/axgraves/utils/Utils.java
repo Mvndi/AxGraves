db01534cc18f84203ad8e3671ab5acd3d389f434
tlm920 <timo@timomcgrath.com>
2025-02-24 21:33:54 -0500

    Custom gravestone item

--- src/main/java/com/artillexstudios/axgraves/utils/Utils.java
+++ src/main/java/com/artillexstudios/axgraves/utils/Utils.java
@@ -3,13 +3,21 @@ package com.artillexstudios.axgraves.utils;
 import com.artillexstudios.axapi.nms.wrapper.ServerPlayerWrapper;
 import com.artillexstudios.axapi.utils.ItemBuilder;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import static com.artillexstudios.axgraves.AxGraves.CONFIG;
+import java.util.function.Consumer;
 
 public class Utils {
+    private static final ItemStack GRAVE_ITEM = make(new ItemStack(Material.STICK), item -> {
+        ItemMeta meta = item.getItemMeta();
+        meta.setItemModel(new NamespacedKey("mvndicraft", "gravestone"));
+        item.setItemMeta(meta);
+    });
 
     @NotNull
     public static ItemStack getPlayerHead(@NotNull OfflinePlayer player) {
@@ -26,5 +34,15 @@ public class Utils {
         if (texture != null) builder.setTextureValue(texture);
 
         return builder.get();
+    }
+
+    @NotNull
+    public static ItemStack getGraveItem() {
+        return GRAVE_ITEM.clone();
+    }
+
+    public static <T> T make(T object, Consumer<? super T> consumer) {
+        consumer.accept(object);
+        return object;
     }
 }
