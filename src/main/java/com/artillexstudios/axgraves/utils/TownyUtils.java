ddf49733b2c4d7eff7327b3eecabe8811989c490
ewof <marroq@marroq.xyz>
2026-02-27 17:33:37 -0500

    fix spam if not debug

--- src/main/java/com/artillexstudios/axgraves/utils/TownyUtils.java
+++ src/main/java/com/artillexstudios/axgraves/utils/TownyUtils.java
@@ -22,7 +22,8 @@ public class TownyUtils {
 
         double distance = deathLocation.distance(spawnLocation);
 
-        LogUtils.debug("Distance from death location to town spawn: " + distance);
+        if (AxGraves.isDebugMode())
+            LogUtils.debug("Distance from death location to town spawn: " + distance);
         return distance;
     }
 
@@ -45,9 +46,10 @@ public class TownyUtils {
         BattleSession battleSession = BattleSession.getBattleSession();
         boolean active = battleSession != null && battleSession.isActive();
 
-        LogUtils.debug(
-                "Checked SiegeWar battle session: " + (battleSession != null ? battleSession.toString() : "null")
-                        + ", active: " + active);
+        if (AxGraves.isDebugMode())
+            LogUtils.debug(
+                    "Checked SiegeWar battle session: " + (battleSession != null ? battleSession.toString() : "null")
+                            + ", active: " + active);
         return active;
     }
 
